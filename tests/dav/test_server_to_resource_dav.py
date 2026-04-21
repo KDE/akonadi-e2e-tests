f41a1e0ec9e6a3b467b57378e75cfe27869d4372
Kenny Lorin <kenny.lorin@enioka.com>
2026-04-21 13:56:43 +0200

    WIP

--- tests/dav/test_server_to_resource_dav.py
+++ tests/dav/test_server_to_resource_dav.py
@@ -1,3 +1,18 @@
 # SPDX-FileCopyrightText: 2026 Dominique MICHEL <dominique.michel@enioka.com>
+# SPDX-FileCopyrightText: 2026 Kenny LORIN <kenny.lorin@enioka.com>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
+import logging
+
+from caldav.collection import Principal
+from src.akonadi.dav_resource import DAVResource
+
+
+log = logging.getLogger("caldav")
+
+def test_add_collection_to_server_is_sync(
+    dav_principal: Principal, groupware_resource: DAVResource
+) -> None:
+    calendars = dav_principal.calendars()
+    log.error(f"hello {calendars=}")
+    # dav_client.calendar(url="http://example.com/some-calendar", parent=None, name="SomeCalendar")
