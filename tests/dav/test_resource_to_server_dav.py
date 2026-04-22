acefc731eaeb9bb6ea94857e2a80b5218771fcdf
Dominique MICHEL <dominique.michel@enioka.com>
2026-04-22 17:34:45 +0200

    Add akonadi dav self-sync remove event test

--- tests/dav/test_resource_to_server_dav.py
+++ tests/dav/test_resource_to_server_dav.py
@@ -5,6 +5,7 @@ import pytest
 from AkonadiCore import Akonadi
 from caldav.collection import Principal
 
+from src.akonadi.client import AkonadiClient
 from src.akonadi.dav_resource import DAVResource
 from src.akonadi.utils import AkonadiUtils
 from src.dav.test_utils import assert_all_collections_are_equals
@@ -71,6 +72,33 @@ def test_akonadi_sync_add_item(dav_principal: Principal, groupware_resource: DAV
 
     wait_until(
         lambda: len(dav_principal.calendar(calendar.name).get_events()) == len(calendar.events) + 1
+    )
+
+    assert_all_collections_are_equals(dav_principal, groupware_resource)
+
+
+def test_akonadi_sync_remove_item(
+    dav_principal: Principal,
+    groupware_resource: DAVResource,
+    akonadi_client: AkonadiClient,
+) -> None:
+    """
+    Removing an item from a collection in the akonadi server, the change is replayed on the server
+    """
+    calendar = DavCalendarFactory.create()
+    groupware_resource.synchronize()
+
+    collection = groupware_resource.collection_from_display_name(calendar.name)
+    items = groupware_resource.list_items(collection.id())
+    item = items[0]
+
+    akonadi_client.delete_item(item.id())
+
+    assert len(groupware_resource.list_items(collection.id())) == len(calendar.events) - 1
+
+    groupware_resource.synchronize()
+    wait_until(
+        lambda: len(dav_principal.calendar(calendar.name).get_events()) == len(calendar.events) - 1
     )
 
     assert_all_collections_are_equals(dav_principal, groupware_resource)
