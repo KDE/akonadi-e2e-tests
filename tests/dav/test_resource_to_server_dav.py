db28cd5d185a15dbdaf762ae3ce3c892276e6cce
Arnaud Chirat <aci@enioka.com>
2026-04-24 17:25:25 +0200

    Add Dav offline test
    Removing an item from a collection in the akonadi server, nothing happens, when the resource is set online, the change is replayed on the server

--- tests/dav/test_resource_to_server_dav.py
+++ tests/dav/test_resource_to_server_dav.py
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2026 Dominique Michel <dominique.michel@enioka.com>
+# SPDX-FileCopyrightText: 2026 Arnaud Chirat <arnaud.chirat@enioka.com>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -11,7 +12,10 @@ from src.akonadi.client import AkonadiClient
 from src.akonadi.dav_resource import DAVResource
 from src.akonadi.test_utils import assert_akonadi_items_are_equal
 from src.akonadi.utils import AkonadiUtils
-from src.dav.test_utils import assert_all_collections_are_equals
+from src.dav.test_utils import (
+    assert_all_collections_are_equals,
+    assert_collection_equal_calendar,
+)
 from src.factories.event_factory import (
     AkonadiCalendarFactory,
     AkonadiEventFactory,
@@ -225,3 +229,34 @@ def test_akonadi_sync_rename_collection(
 
     # Check the items are matching between resource and server
     assert_all_collections_are_equals(dav_principal, groupware_resource)
+
+
+def test_akonadi_offline_remove_item(
+    dav_principal: Principal,
+    groupware_resource: DAVResource,
+    akonadi_client: AkonadiClient,
+) -> None:
+    """
+    Removing an item from a collection in the akonadi server, nothing happens, when the resource is set online, the change is replayed on the server
+    """
+    calendar = DavCalendarFactory.create()
+    groupware_resource.synchronize()
+
+    collection = groupware_resource.collection_from_display_name(calendar.name)
+    items = groupware_resource.list_items(collection.id())
+    item_to_delete = items[0]
+
+    assert_collection_equal_calendar(collection.remoteId(), groupware_resource, dav_principal)
+
+    groupware_resource.set_online(False)
+    akonadi_client.delete_item(item_to_delete.id())
+
+    # assert nothing happens
+    assert len(dav_principal.calendar(calendar.name).get_events()) == len(calendar.events)
+
+    # Synchronize is done when the resource is set online on Dav-Implementation, so no need to call synchronize()
+    groupware_resource.set_online(True)
+
+    # item should be removed after synchronization
+    assert len(dav_principal.calendar(calendar.name).get_events()) == len(calendar.events) - 1
+    assert_collection_equal_calendar(collection.remoteId(), groupware_resource, dav_principal)
