b9e4198172a2ac25bd9b645392b3ca3ece6b5bb1
Dominique MICHEL <dominique.michel@enioka.com>
2026-04-24 09:28:27 +0200

    Homogeinise my name in the copyrights

--- tests/dav/test_resource_to_server_dav.py
+++ tests/dav/test_resource_to_server_dav.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2026 MICHEL Dominique <dominique.michel@enioka.com>
+# SPDX-FileCopyrightText: 2026 Dominique Michel <dominique.michel@enioka.com>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
