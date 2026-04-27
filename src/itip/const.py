3bdbf3e190d28fa930e41e9665aec669c7232aa8
Dominique MICHEL <dominique.michel@enioka.com>
2026-04-27 16:57:58 +0200

    Add itip process message utility

---
+++ src/itip/const.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2026 Dominique Michel <dominique.michel@enioka.com>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+ITIP_ACTION_ACCEPTED = "accepted"
+ITIP_ACTION_DECLINED = "declined"
+ITIP_ACTION_TENTATIVE = "tentative"
+ITIP_ACTION_DELEGATED = "delegated"
+ITIP_ACTION_COUNTER = "counter"
