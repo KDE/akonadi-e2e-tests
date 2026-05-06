d5ccb9a91a1fb65877b1bbbb6e23a076fbc61aa7
Noham Devillers <noham.devillers@enioka.com>
2026-05-06 17:12:22 +0200

    Add dav conflict test on collection removed

--- tests/dav/test_conflict_dav.py
+++ tests/dav/test_conflict_dav.py
@@ -1,15 +1,22 @@
 # SPDX-FileCopyrightText: 2026 Dominique MICHEL <dominique.michel@enioka.com>
+# SPDX-FileCopyrightText: 2026 Noham Devillers <noham.devillers@enioka.com>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
+
+from logging import getLogger
 
 import pytest
 from caldav.collection import Principal
 from caldav.elements import dav
 from caldav.lib.error import NotFoundError
 
+from src.akonadi.client import AkonadiClient
 from src.akonadi.dav_resource import DAVResource
 from src.dav.test_utils import assert_all_collections_are_equals
 from src.factories.event_factory import DavCalendarFactory, DavEventFactory, fake
+from src.test import wait_until
+
+log = getLogger(__name__)
 
 
 @pytest.mark.xfail(
@@ -81,3 +88,40 @@ def test_offline_rename_collection_server_and_resource(
     assert groupware_resource.collection_from_display_name(server_new_name)
     assert dav_principal.calendar(server_new_name)
     assert_all_collections_are_equals(dav_principal, groupware_resource)
+
+
+def test_conflict_remove_collection(
+    dav_principal: Principal, groupware_resource: DAVResource, akonadi_client: AkonadiClient
+) -> None:
+    """
+    Removing an item from a collection in the akonadi server, removing the collection on the server, nothing happens
+    When the resource is set online, the collection is removed from the akonadi server
+    """
+    calendar = DavCalendarFactory.create()
+    groupware_resource.synchronize()
+
+    collection = groupware_resource.collection_from_display_name(calendar.name)
+    items = groupware_resource.list_items(collection.name())
+    assert len(items) > 0
+    item = items[0]
+
+    groupware_resource.set_online(False)
+
+    akonadi_client.delete_item(item.id())
+
+    dav_calendar = dav_principal.calendar(calendar.name)
+
+    dav_calendar.delete()
+    wait_until(
+        lambda: calendar.name not in [c.get_display_name() for c in dav_principal.get_calendars()]
+    )
+
+    assert collection.id() in [c.id() for c in groupware_resource.list_collections()]
+    assert len(groupware_resource.list_items(collection.id())) == len(calendar.events) - 1
+    assert item.id() not in [i.id() for i in groupware_resource.list_items(collection.name())]
+
+    groupware_resource.set_online(True)
+
+    # assert the collection is deleted both in akonadi and in the server
+    assert calendar.name not in [c.get_display_name() for c in dav_principal.get_calendars()]
+    assert collection.id() not in [c.id() for c in groupware_resource.list_collections()]
