859df6fc213ac7f5729074b4ff42b4d9fe1f2d56
Dominique MICHEL <dominique.michel@enioka.com>
2026-05-06 16:33:29 +0200

    Add DAV delete collection and add event test

--- tests/dav/test_conflict_dav.py
+++ tests/dav/test_conflict_dav.py
@@ -1,3 +1,45 @@
 # SPDX-FileCopyrightText: 2026 Dominique MICHEL <dominique.michel@enioka.com>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
+import pytest
+from caldav.collection import Principal
+from caldav.lib.error import NotFoundError
+
+from src.akonadi.dav_resource import DAVResource
+from src.dav.test_utils import assert_all_collections_are_equals
+from src.factories.event_factory import DavCalendarFactory, DavEventFactory
+
+
+@pytest.mark.xfail(
+    reason="RADICALE: Collection is recreated with an item. https://invent.kde.org/pim/pim-technical-roadmap/-/work_items/102"
+)
+def test_offline_remove_collection_and_add_event(
+    dav_principal: Principal, groupware_resource: DAVResource
+) -> None:
+    """
+    Removing a collection from the akonadi server, adding an item to the collection on the server, nothing happens
+    When the resource is set online, the change is replayed on the server and the collection is removed (including the newly added item)
+    """
+    calendar = DavCalendarFactory.create()
+    groupware_resource.synchronize()
+    assert_all_collections_are_equals(dav_principal, groupware_resource)
+    initial_collections = groupware_resource.list_collections()
+    collection = groupware_resource.collection_from_display_name(calendar.name)
+
+    groupware_resource.set_online(False)
+
+    groupware_resource.delete_collection(collection.name())
+    DavEventFactory.create(calendar=calendar.name)
+
+    # Nothing happens
+    assert dav_principal.calendar(calendar.name)
+    assert len(groupware_resource.list_collections()) == len(initial_collections) - 1
+    assert calendar.name not in [c.displayName() for c in groupware_resource.list_collections()]
+
+    groupware_resource.set_online(True)
+
+    with pytest.raises(NotFoundError):
+        dav_principal.calendar(calendar.name)
+    assert len(groupware_resource.list_collections()) == len(initial_collections) - 1
+    assert calendar.name not in [c.displayName() for c in groupware_resource.list_collections()]
+    assert_all_collections_are_equals(dav_principal, groupware_resource)
